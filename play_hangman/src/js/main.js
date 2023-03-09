870c7b3a9b7ffa9be061e095ef13b59f36ab19f0
kabalidevercan <kabalidevercan@gmail.com>
2023-03-09 18:44:16 +0300

    third-project

--- play_hangman/src/js/main.js
+++ play_hangman/src/js/main.js
@@ -1,15 +1,20 @@
 const word_element = document.getElementById('word');
-const correctLetters = ['j', 'a', 'v'];
+const correctLetters = [];
 const wrongLetters = [];
+const popup = document.getElementById('popup-container');
+const message_el = document.getElementById('success-message');
+let selectedWord = getRandomWord();
+const wrongLetters_el = document.getElementById('wrong-letters');
+const items = document.querySelectorAll('.item');
+const message = document.getElementById('message');
+const playAgainButton = document.getElementById('play-again');
 
 function getRandomWord() {
-  const words = ['javascript', 'java', 'python'];
+  const words = ['javascript', 'java', 'python', 'css', 'kabal', 'nodejs'];
   return words[Math.floor(Math.random() * words.length)];
 }
 
 function displayWord() {
-  const selectedWord = getRandomWord();
-
   word_element.innerHTML = `${selectedWord
     .split('')
     .map(
@@ -23,8 +28,68 @@ function displayWord() {
 
   const w = word_element.innerText.replace(/\n/g, '');
   if (w === selectedWord) {
-    console.log('bildiniz..');
+    popup.style.display = 'flex';
+    message_el.innerText = 'Tebrikler Kazandınız';
   }
 }
+
+function updateWrongLetters() {
+  wrongLetters_el.innerHTML = `
+   ${wrongLetters.length > 0 ? '<h3>Hatali Harfler</h3>' : ''}
+   ${wrongLetters.map(letter => `<span> ${letter}</span>`)}
+  `;
+
+  items.forEach((item, index) => {
+    const errorCount = wrongLetters.length;
+    if (index < errorCount) {
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+
+  if (wrongLetters.length === items.length) {
+    popup.style.display = 'flex';
+    message_el.innerText = 'Maalesef Kaybettiniz.';
+  }
+}
+
+function displayMessage() {
+  message.classList.add('show');
+
+  setTimeout(function () {
+    message.classList.remove('show');
+  }, 1000);
+}
+
+window.addEventListener('keydown', function (e) {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        displayMessage();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLetters();
+      } else {
+        displayMessage();
+      }
+    }
+  }
+});
+
+playAgainButton.addEventListener('click', function () {
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+  selectedWord = getRandomWord();
+  displayWord();
+  updateWrongLetters();
+  popup.style.display = 'none';
+});
 
 displayWord();
