f0a9f9f6771a999812cf69d59d4f42d9144a49ff
kabalidevercan <kabalidevercan@gmail.com>
2023-03-22 22:14:34 +0300

    mini-translate

---
+++ mini_translate/src/js/main.js
@@ -0,0 +1,6 @@
+const ul = document.querySelector('#yarrak');
+const button = document.querySelector('#yarram');
+
+button.addEventListener('click', function () {
+  ul.classList.toggle('hidden');
+});
